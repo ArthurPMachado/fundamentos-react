ab05b837e8cbef307c8bf7910e2199925aada26e
Arthur Machado <machadoparthur1@gmail.com>
2024-09-01 18:22:20 -0300

    feat: explore props concept

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ export function App() {
 
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Arthur Pereira Machado" 
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo fuga excepturi ducimus. Excepturi sequi repellendus assumenda quaerat voluptatibus beatae iure ab praesentium quas qui fuga fugit, nihil, rem omnis officia?"
+      />
+      <Post 
+        author="Teste" 
+        content="Um conteudo de teste para exibição"
+      />
     </div>
   )
 }
