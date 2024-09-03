53bca30f97205fa991a6e0ecf65d4d23a9b6de47
Arthur Machado <machadoparthur1@gmail.com>
2024-09-03 12:43:38 -0300

    feat: finish sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header";
+import { Sidebar } from './components/Sidebar';
 import { Post } from "./Post";
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -8,14 +11,19 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Arthur Pereira Machado" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo fuga excepturi ducimus. Excepturi sequi repellendus assumenda quaerat voluptatibus beatae iure ab praesentium quas qui fuga fugit, nihil, rem omnis officia?"
-      />
-      <Post 
-        author="Teste" 
-        content="Um conteudo de teste para exibição"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Arthur Pereira Machado" 
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo fuga excepturi ducimus. Excepturi sequi repellendus assumenda quaerat voluptatibus beatae iure ab praesentium quas qui fuga fugit, nihil, rem omnis officia?"
+          />
+          <Post 
+            author="Teste" 
+            content="Um conteudo de teste para exibição"
+          />
+        </main>
+      </div>
     </div>
   )
 }
