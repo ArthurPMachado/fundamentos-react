7e63a6200eeb1feca69e98edf9b1d7974e4e87bc
Arthur Machado <machadoparthur1@gmail.com>
2024-09-03 00:58:03 -0300

    feat: start header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 export function App() {
 
   return (
     <div>
+      <Header />
       <Post 
         author="Arthur Pereira Machado" 
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo fuga excepturi ducimus. Excepturi sequi repellendus assumenda quaerat voluptatibus beatae iure ab praesentium quas qui fuga fugit, nihil, rem omnis officia?"
