c94cd421b3f113eb1bb8359cdb10b8bca7205ac9
Arthur Machado <machadoparthur1@gmail.com>
2024-09-05 23:53:32 -0300

    feat: create avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,6 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css'
 
 export function Post() {
@@ -6,10 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img 
-            className={styles.avatar} 
-            src="https://github.com/ArthurPMachado.png" 
-          />
+          <Avatar src="https://github.com/ArthurPMachado.png" />
           <div className={styles.authorInfo}>
             <strong>Arthur Pereira Machado</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { PencilLine } from '@phosphor-icons/react'
 
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -10,7 +12,8 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/ArthurPMachado.png" />
+        <Avatar src="https://github.com/ArthurPMachado.png" />
+
         <strong>Arthur Pereira Machado</strong>
         <span>Web Developer</span>
       </div>
